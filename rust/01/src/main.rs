7c2d0dfa1a057259b9e3b8c70ca53bdb0769b778
Maurizio De Santis <desantis.maurizio@gmail.com>
2024-12-01 17:46:16 +0100

    Add solution for day 1 part 2

--- rust/01/src/main.rs
+++ rust/01/src/main.rs
@@ -1,5 +1,6 @@
-fn main() {
+fn part_1() {
     let input_contents = include_str!("../../../inputs/01/input");
+
     let mut lefts = Vec::new();
     let mut rights = Vec::new();
     let mut distances_sum = 0;
@@ -24,5 +25,37 @@ fn main() {
         distances_sum += distance;
     }
 
-    println!("distances_sum: {distances_sum:?}");
+    println!("distances sum: {distances_sum:?}");
+}
+
+fn part_2() {
+    let input_contents = include_str!("../../../inputs/01/input");
+
+    let mut lefts = Vec::new();
+    let mut rights = Vec::new();
+    let mut similarity_score = 0;
+
+    for line in input_contents.lines() {
+        for (i, slice) in line.split_whitespace().enumerate() {
+            let value = slice.parse::<i32>().unwrap();
+
+            match i % 2 {
+                0 => lefts.push(value),
+                _ => rights.push(value),
+            }
+        }
+    }
+
+    for left in lefts.iter() {
+        let count = rights.iter().filter(|&right| right == left).count();
+
+        similarity_score += left * (count as i32);
+    }
+
+    println!("similarity score: {similarity_score:?}");
+}
+
+fn main() {
+    part_1();
+    part_2();
 }
