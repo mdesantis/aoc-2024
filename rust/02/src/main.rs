995b931f5d58b2454b93ddbf1f62fa0c6fcd7eab
Maurizio De Santis <desantis.maurizio@gmail.com>
2024-12-02 16:41:11 +0100

    Add solution for day 2 part 2

--- rust/02/src/main.rs
+++ rust/02/src/main.rs
@@ -2,7 +2,7 @@ const INPUT_CONTENTS: &str = include_str!("../../../inputs/02/input");
 
 use std::cmp::Ordering;
 
-fn is_report_safe(levels: Vec<i32>) -> bool {
+fn is_report_safe(levels: &Vec<i32>) -> bool {
     let mut maybe_prev_level: Option<i32> = None;
     let mut maybe_prev_report_ordering: Option<Ordering> = None;
     let mut report_ordering;
@@ -33,7 +33,17 @@ fn is_report_safe(levels: Vec<i32>) -> bool {
     true
 }
 
-fn safe_reports_amount(input_contents: &str) -> i32 {
+fn problem_dampener_levels<'a>(levels: &'a Vec<i32>) -> impl Iterator<Item = Vec<i32>> + 'a {
+    (0..levels.len()).map(|i| {
+        let mut problem_dampener_level = (*levels).clone();
+
+        problem_dampener_level.remove(i);
+
+        problem_dampener_level
+    })
+}
+
+fn safe_reports_amount(input_contents: &str, problem_dampener_module_enabled: bool) -> i32 {
     input_contents
         .lines()
         .filter(|line| {
@@ -42,14 +52,31 @@ fn safe_reports_amount(input_contents: &str) -> i32 {
                 .map(|slice| slice.parse::<i32>().unwrap())
                 .collect::<Vec<_>>();
 
-            is_report_safe(levels)
+            match problem_dampener_module_enabled {
+                false => is_report_safe(&levels),
+                _ => {
+                    is_report_safe(&levels)
+                        || problem_dampener_levels(&levels).any(|levels| is_report_safe(&levels))
+                }
+            }
         })
         .count() as i32
 }
 
 fn main() {
-    let safe_reports_amount = safe_reports_amount(INPUT_CONTENTS);
-    println!("Safe reports amount: {safe_reports_amount}")
+    let safe_reports_amount_with_problem_dampener_module_disabled =
+        safe_reports_amount(INPUT_CONTENTS, false);
+    println!(
+        "Safe reports amount with problem dampener module disabled: {}",
+        safe_reports_amount_with_problem_dampener_module_disabled
+    );
+
+    let safe_reports_amount_with_problem_dampener_module_enabled =
+        safe_reports_amount(INPUT_CONTENTS, true);
+    println!(
+        "Safe reports amount with problem dampener module enabled: {}",
+        safe_reports_amount_with_problem_dampener_module_enabled
+    )
 }
 
 #[cfg(test)]
@@ -65,9 +92,16 @@ mod tests {
 ";
 
     #[test]
-    fn test_safe_reports_amount() {
+    fn test_safe_reports_amount_with_problem_dampener_module_disabled() {
         let expected = 2;
-        let actual = safe_reports_amount(TEST_INPUT_CONTENTS);
+        let actual = safe_reports_amount(TEST_INPUT_CONTENTS, false);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_safe_reports_amount_with_problem_dampener_module_enabled() {
+        let expected = 4;
+        let actual = safe_reports_amount(TEST_INPUT_CONTENTS, true);
         assert_eq!(expected, actual);
     }
 }
