8c367318a293cbca3c8593748b76081fdb071a73
Maurizio De Santis <desantis.maurizio@gmail.com>
2024-12-02 15:38:53 +0100

    Functional refactoring

--- rust/02/src/main.rs
+++ rust/02/src/main.rs
@@ -2,19 +2,15 @@ const INPUT_CONTENTS: &str = include_str!("../../../inputs/02/input");
 
 use std::cmp::Ordering;
 
-fn is_report_safe(line: &str) -> bool {
-    let levels = line
-        .split_whitespace()
-        .map(|slice| slice.parse::<i32>().unwrap());
-
+fn is_report_safe(levels: Vec<i32>) -> bool {
     let mut maybe_prev_level: Option<i32> = None;
     let mut maybe_prev_report_ordering: Option<Ordering> = None;
     let mut report_ordering;
 
-    for (i, level) in levels.enumerate() {
+    for (i, level) in levels.iter().enumerate() {
         if i > 0 {
             let prev_level = maybe_prev_level.unwrap();
-            report_ordering = prev_level.cmp(&level);
+            report_ordering = prev_level.cmp(level);
 
             if report_ordering == Ordering::Equal {
                 return false;
@@ -31,22 +27,24 @@ fn is_report_safe(line: &str) -> bool {
             maybe_prev_report_ordering = Some(report_ordering);
         }
 
-        maybe_prev_level = Some(level);
+        maybe_prev_level = Some(*level);
     }
 
     true
 }
 
 fn safe_reports_amount(input_contents: &str) -> i32 {
-    let mut safe_reports_amount = 0;
+    input_contents
+        .lines()
+        .filter(|line| {
+            let levels = line
+                .split_whitespace()
+                .map(|slice| slice.parse::<i32>().unwrap())
+                .collect::<Vec<_>>();
 
-    for line in input_contents.lines() {
-        if is_report_safe(line) {
-            safe_reports_amount += 1
-        }
-    }
-
-    safe_reports_amount
+            is_report_safe(levels)
+        })
+        .count() as i32
 }
 
 fn main() {
