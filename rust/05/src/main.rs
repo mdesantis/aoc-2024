5bbb0c8695f864823ff125f8dd54a23c68e255fe
Maurizio De Santis <desantis.maurizio@gmail.com>
2024-12-05 20:56:11 +0100

    Refactoring

--- rust/05/src/main.rs
+++ rust/05/src/main.rs
@@ -2,35 +2,29 @@ use std::{cmp::Ordering, collections::HashMap};
 
 const INPUT_CONTENTS: &str = include_str!("../../../inputs/05/input");
 
-fn collect_rules(rules_input: &str) -> HashMap<i32, Vec<i32>> {
+fn collect_rules(rules_input: &str) -> HashMap<&str, Vec<&str>> {
     let mut rules = HashMap::new();
 
     for line in rules_input.lines() {
         let mut split = line.split("|");
-        let (greater, lesser) = (
-            split.next().unwrap().parse::<i32>().unwrap(),
-            split.next().unwrap().parse::<i32>().unwrap(),
-        );
+        let (greater, less) = (split.next().unwrap(), split.next().unwrap());
 
         rules
             .entry(greater)
-            .and_modify(|entry: &mut Vec<_>| entry.push(lesser))
-            .or_insert(vec![lesser]);
+            .and_modify(|entry: &mut Vec<_>| entry.push(less))
+            .or_insert(vec![less]);
     }
 
     rules
 }
 
-fn collect_updates_line_values(updates_line: &str) -> Vec<i32> {
-    updates_line
-        .split(",")
-        .map(|v| v.parse::<i32>().unwrap())
-        .collect::<Vec<_>>()
+fn collect_updates_line_values(updates_line: &str) -> Vec<&str> {
+    updates_line.split(",").collect::<Vec<_>>()
 }
 
 fn are_updates_line_values_sorted(
-    updates_line_values: &[i32],
-    rules: &HashMap<i32, Vec<i32>>,
+    updates_line_values: &[&str],
+    rules: &HashMap<&str, Vec<&str>>,
 ) -> bool {
     updates_line_values.is_sorted_by(|a, b| match rules.get(b) {
         Some(b_rule_values) => b_rule_values.iter().all(|v| *v != *a),
@@ -49,15 +43,19 @@ fn correctly_ordered_updates_middle_pages_sum(input_contents: &str) -> i32 {
             let updates_line_values = collect_updates_line_values(line);
             let is_sorted = are_updates_line_values_sorted(&updates_line_values, &rules);
 
-            is_sorted.then(|| updates_line_values[updates_line_values.len() / 2])
+            is_sorted.then(|| {
+                updates_line_values[updates_line_values.len() / 2]
+                    .parse::<i32>()
+                    .unwrap()
+            })
         })
         .sum::<i32>()
 }
 
-fn sort_updates_line_values(a: &i32, b: &i32, rules: &HashMap<i32, Vec<i32>>) -> Ordering {
+fn sort_updates_line_values(a: &str, b: &str, rules: &HashMap<&str, Vec<&str>>) -> Ordering {
     match (rules.get(a), rules.get(b)) {
-        (Some(a_rule_values), _) if a_rule_values.iter().any(|v| *v == *b) => Ordering::Greater,
-        (_, Some(b_rule_values)) if b_rule_values.iter().any(|v| *v == *a) => Ordering::Less,
+        (Some(a_rule_values), _) if a_rule_values.iter().any(|v| *v == b) => Ordering::Greater,
+        (_, Some(b_rule_values)) if b_rule_values.iter().any(|v| *v == a) => Ordering::Less,
         _ => Ordering::Equal,
     }
 }
@@ -77,6 +75,8 @@ fn reordered_wrongly_ordered_updates_middle_pages_sum(input_contents: &str) -> i
                 updates_line_values.sort_by(|a, b| sort_updates_line_values(a, b, rules));
 
                 updates_line_values[updates_line_values.len() / 2]
+                    .parse::<i32>()
+                    .unwrap()
             })
         })
         .sum::<i32>()
