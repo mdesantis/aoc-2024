a12a1c6f772461e426af6a5d7422b5ea0198ce3e
Maurizio De Santis <desantis.maurizio@gmail.com>
2024-12-06 17:38:00 +0100

    Add solution for day 6 part 2 (but it has an error)
    The expected value for the provided input is 1719,
    but this solution returns 1720. To be honest, I'm
    stuck.

--- rust/06/src/main.rs
+++ rust/06/src/main.rs
@@ -1,8 +1,9 @@
 use std::collections::HashSet;
 
 const INPUT_CONTENTS: &str = include_str!("../../../inputs/06/input");
+const OBSTRUCTED_TILE: char = '#';
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
 enum GuardDirection {
     Top,
     Right,
@@ -16,9 +17,6 @@ impl TryFrom<char> for GuardDirection {
     fn try_from(char: char) -> Result<Self, Self::Error> {
         match char {
             '^' => Ok(Self::Top),
-            '>' => Ok(Self::Right),
-            'v' => Ok(Self::Down),
-            '<' => Ok(Self::Left),
             _ => Err(()),
         }
     }
@@ -39,8 +37,7 @@ fn get_map_and_starting_values(
                 .fold(Vec::new(), |mut acc, (y, char)| {
                     if curr_pos.is_none() {
                         if let Ok(direction) = GuardDirection::try_from(char) {
-                            curr_pos = Some((x, y));
-                            curr_dir = Some(direction);
+                            (curr_pos, curr_dir) = (Some((x, y)), Some(direction));
                         }
                     }
 
@@ -53,7 +50,7 @@ fn get_map_and_starting_values(
     (map, curr_pos.unwrap(), curr_dir.unwrap())
 }
 
-fn next_dir(curr_dir: &GuardDirection) -> GuardDirection {
+fn next_dir(curr_dir: GuardDirection) -> GuardDirection {
     match curr_dir {
         GuardDirection::Top => GuardDirection::Right,
         GuardDirection::Right => GuardDirection::Down,
@@ -63,7 +60,7 @@ fn next_dir(curr_dir: &GuardDirection) -> GuardDirection {
 }
 
 fn is_obstructed(tile: char) -> bool {
-    tile == '#'
+    tile == OBSTRUCTED_TILE
 }
 
 fn maybe_next_values(
@@ -71,47 +68,100 @@ fn maybe_next_values(
     rows: usize,
     cols: usize,
     curr_pos: (usize, usize),
-    curr_dir: &GuardDirection,
+    curr_dir: GuardDirection,
 ) -> Option<((usize, usize), GuardDirection)> {
     let (curr_x, curr_y) = curr_pos;
     let (maybe_next_x, maybe_next_y) = match curr_dir {
-        GuardDirection::Top => (curr_x.checked_sub(1), Some(curr_y)),
-        GuardDirection::Right => (Some(curr_x), curr_y.checked_add(1)),
-        GuardDirection::Down => (curr_x.checked_add(1), Some(curr_y)),
-        GuardDirection::Left => (Some(curr_x), curr_y.checked_sub(1)),
+        GuardDirection::Top if curr_x > 0 => (Some(curr_x - 1), Some(curr_y)),
+        GuardDirection::Right => (Some(curr_x), Some(curr_y + 1)),
+        GuardDirection::Down => (Some(curr_x + 1), Some(curr_y)),
+        GuardDirection::Left if curr_y > 0 => (Some(curr_x), Some(curr_y - 1)),
+        _ => (None, None),
     };
     match (maybe_next_x, maybe_next_y) {
         (None, _) => None,
         (_, None) => None,
         (Some(next_x), Some(next_y)) if next_x >= rows || next_y >= cols => None,
         (Some(next_x), Some(next_y)) if is_obstructed(map[next_x][next_y]) => {
-            maybe_next_values(map, rows, cols, curr_pos, &next_dir(curr_dir))
+            maybe_next_values(map, rows, cols, curr_pos, next_dir(curr_dir))
         }
-        (Some(next_x), Some(next_y)) => Some(((next_x, next_y), *curr_dir)),
+        (Some(next_x), Some(next_y)) => Some(((next_x, next_y), curr_dir)),
     }
 }
 
-fn visited_positions_amount(input_contents: &str) -> i32 {
-    let (map, mut curr_pos, mut curr_dir) = get_map_and_starting_values(input_contents);
+fn visited_positions(
+    map: &Vec<Vec<char>>,
+    mut curr_pos: (usize, usize),
+    mut curr_dir: GuardDirection,
+) -> HashSet<(usize, usize)> {
     let mut visited_positions = HashSet::new();
     let rows = map.len();
     let cols = map[0].len();
 
     visited_positions.insert(curr_pos);
 
-    while let Some((next_pos, next_dir)) = maybe_next_values(&map, rows, cols, curr_pos, &curr_dir)
-    {
-        curr_pos = next_pos;
-        curr_dir = next_dir;
+    while let Some((next_pos, next_dir)) = maybe_next_values(&map, rows, cols, curr_pos, curr_dir) {
+        (curr_pos, curr_dir) = (next_pos, next_dir);
+
         visited_positions.insert(curr_pos);
     }
 
+    visited_positions
+}
+
+fn visited_positions_amount(visited_positions: &HashSet<(usize, usize)>) -> i32 {
     visited_positions.len() as i32
 }
 
+fn is_stuck_in_loop(
+    map: Vec<Vec<char>>,
+    rows: usize,
+    cols: usize,
+    mut curr_pos: (usize, usize),
+    mut curr_dir: GuardDirection,
+) -> bool {
+    let mut visited_positions_and_direction = HashSet::new();
+    let initial_pos = curr_pos;
+
+    while let Some((next_pos, next_dir)) = maybe_next_values(&map, rows, cols, curr_pos, curr_dir) {
+        (curr_pos, curr_dir) = (next_pos, next_dir);
+
+        if initial_pos != curr_pos && !visited_positions_and_direction.insert((curr_pos, curr_dir))
+        {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn stuck_in_loop_amount(
+    map: Vec<Vec<char>>,
+    visited_positions: &HashSet<(usize, usize)>,
+    curr_pos: (usize, usize),
+    curr_dir: GuardDirection,
+) -> i32 {
+    let rows = map.len();
+    let cols = map[0].len();
+    let stuck_in_loop = visited_positions.iter().filter(|(x, y)| {
+        let mut map = map.clone();
+
+        map[*x][*y] = OBSTRUCTED_TILE;
+        is_stuck_in_loop(map, rows, cols, curr_pos, curr_dir)
+    });
+
+    stuck_in_loop.count() as i32
+}
+
 fn main() {
-    let result = visited_positions_amount(INPUT_CONTENTS);
+    let (map, curr_pos, curr_dir) = get_map_and_starting_values(INPUT_CONTENTS);
+    let visited_positions = visited_positions(&map, curr_pos, curr_dir);
+
+    let result = visited_positions_amount(&visited_positions);
     println!("Visited positions amount: {result}");
+
+    let result = stuck_in_loop_amount(map, &visited_positions, curr_pos, curr_dir);
+    println!("Stuck in loop amount: {result}");
 }
 
 #[cfg(test)]
@@ -133,8 +183,31 @@ mod tests {
     #[test]
     fn test_visited_positions_amount() {
         let expected = 41;
-        let actual = visited_positions_amount(TEST_INPUT_CONTENTS);
+        let (map, curr_pos, curr_dir) = get_map_and_starting_values(TEST_INPUT_CONTENTS);
+        let visited_positions = visited_positions(&map, curr_pos, curr_dir);
+        let actual = visited_positions_amount(&visited_positions);
 
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_stuck_in_loop_amount() {
+        let expected = 6;
+        let (map, curr_pos, curr_dir) = get_map_and_starting_values(TEST_INPUT_CONTENTS);
+        let visited_positions = visited_positions(&map, curr_pos, curr_dir);
+        let actual = stuck_in_loop_amount(map, &visited_positions, curr_pos, curr_dir);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_stuck_in_loop_amount_with_real_input() {
+        let expected = 1719;
+        let (map, curr_pos, curr_dir) = get_map_and_starting_values(INPUT_CONTENTS);
+        let visited_positions = visited_positions(&map, curr_pos, curr_dir);
+        let actual = stuck_in_loop_amount(map, &visited_positions, curr_pos, curr_dir);
+
+        // The actual value is 1720 for some reason :-(
         assert_eq!(expected, actual);
     }
 }
