9b952e9ae7fdf7369e1396fe6976c7630d637f2a
Maurizio De Santis <desantis.maurizio@gmail.com>
2024-12-07 17:55:11 +0100

    Add solution for day 7 part 1

---
+++ rust/07/src/main.rs
@@ -0,0 +1,91 @@
+#![feature(fn_traits)]
+
+const INPUT_CONTENTS: &str = include_str!("../../../inputs/07/input");
+
+fn combinations<'a, T: 'a + Copy>(a: &'a [T], n: usize) -> Vec<Vec<T>> {
+    if n == 0 {
+        return vec![vec![]];
+    }
+
+    let mut result = Vec::new();
+
+    for v in a {
+        let mut sub_combinations = combinations(a, n - 1);
+        for combination in &mut sub_combinations {
+            combination.push(*v);
+        }
+        result.extend(sub_combinations);
+    }
+
+    result
+}
+
+fn total_calibration_result(input_contents: &str) -> i64 {
+    let operations = vec![<i64 as std::ops::Add>::add, <i64 as std::ops::Mul>::mul];
+    let mut sum = 0;
+
+    for line in input_contents.lines() {
+        if let Some((result_string, test_values_string)) = line.split_once(':') {
+            let result = result_string.parse::<i64>().unwrap();
+            let test_values = test_values_string
+                .split_whitespace()
+                .map(|v| v.parse::<i64>().unwrap())
+                .collect::<Vec<_>>();
+            let combinations = combinations(&operations, test_values.len() - 1);
+            let mut is_equation_correct = false;
+
+            for operations in combinations {
+                let mut operations_iter = operations.iter();
+                let test_result = test_values
+                    .clone()
+                    .into_iter()
+                    .reduce(|acc, test_value| {
+                        let operation = operations_iter.next().unwrap();
+                        let value = operation.call((acc, test_value));
+                        value
+                    })
+                    .unwrap();
+
+                if test_result == result {
+                    is_equation_correct = true;
+                    break;
+                }
+            }
+
+            if is_equation_correct {
+                sum += result;
+            }
+        }
+    }
+
+    sum
+}
+
+fn main() {
+    let result = total_calibration_result(INPUT_CONTENTS);
+    println!("Total calibration result: {result}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT_CONTENTS: &str = "190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+";
+
+    #[test]
+    fn test_total_calibration_result() {
+        let expected = 3749_i64;
+        let actual = total_calibration_result(TEST_INPUT_CONTENTS);
+
+        assert_eq!(expected, actual);
+    }
+}
