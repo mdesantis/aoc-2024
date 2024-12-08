4218274fad3e0cb5b6296369603a4093267ad9f2
Maurizio De Santis <desantis.maurizio@gmail.com>
2024-12-08 18:05:29 +0100

    Refactoring

--- rust/08/src/main.rs
+++ rust/08/src/main.rs
@@ -20,13 +20,12 @@ fn antennas(lines: &[&str]) -> Vec<(i32, i32, char)> {
         .collect::<Vec<_>>()
 }
 
-fn antennas_pair_to_in_bound_antinodes(
-    antenna1: (i32, i32),
-    antenna2: (i32, i32),
+fn antennas_pair_to_antinodes(
+    antennas_pair: ((i32, i32), (i32, i32)),
     rows: i32,
     cols: i32,
 ) -> Option<Vec<(i32, i32)>> {
-    let ((x1, y1), (x2, y2)) = (antenna1, antenna2);
+    let ((x1, y1), (x2, y2)) = antennas_pair;
     let (dx, dy) = (2 * (x2 - x1), 2 * (y2 - y1));
     let antinodes = vec![(x1 + dx, y1 + dy), (x2 - dx, y2 - dy)];
     let in_bound_antinodes = antinodes
@@ -38,37 +37,36 @@ fn antennas_pair_to_in_bound_antinodes(
     (!in_bound_antinodes.is_empty()).then_some(in_bound_antinodes)
 }
 
-fn in_bound_antinodes(antennas: &[(i32, i32, char)], rows: i32, cols: i32) -> HashSet<(i32, i32)> {
+fn antinodes(antennas: &[(i32, i32, char)], rows: i32, cols: i32) -> HashSet<(i32, i32)> {
     antennas
         .iter()
         .flat_map(move |antenna1| {
-            antennas.iter().filter_map(move |antenna2| {
-                // antennas_pair_to_in_bound_antinodes(*antenna1, *antenna2, rows, cols)
-                match (antenna1, antenna2) {
+            antennas
+                .iter()
+                .filter_map(move |antenna2| match (antenna1, antenna2) {
                     ((x1, y1, c1), (x2, y2, c2)) if c1 == c2 && (x1, y1) < (x2, y2) => {
-                        antennas_pair_to_in_bound_antinodes((*x1, *y1), (*x2, *y2), rows, cols)
+                        antennas_pair_to_antinodes(((*x1, *y1), (*x2, *y2)), rows, cols)
                     }
                     _ => None,
-                }
-            })
+                })
         })
         .flatten()
         .collect()
 }
 
-fn in_bound_antinodes_amount(input_contents: &str) -> i32 {
+fn antinodes_amount(input_contents: &str) -> i32 {
     let lines = input_contents.lines().collect::<Vec<_>>();
     let cols = lines.len() as i32;
     let rows = lines[0].chars().collect::<Vec<_>>().len() as i32;
     let antennas = antennas(&lines);
-    let in_bound_antinodes = in_bound_antinodes(&antennas, rows, cols);
+    let in_bound_antinodes = antinodes(&antennas, rows, cols);
 
     in_bound_antinodes.len() as i32
 }
 
 fn main() {
-    let result = in_bound_antinodes_amount(INPUT_CONTENTS);
-    println!("In bound antinodes amount: {result}");
+    let result = antinodes_amount(INPUT_CONTENTS);
+    println!("Antinodes amount: {result}");
 }
 
 #[cfg(test)]
@@ -92,7 +90,7 @@ mod tests {
     #[test]
     fn test_in_bound_antinodes_amount() {
         let expected = 14;
-        let actual = in_bound_antinodes_amount(TEST_INPUT_CONTENTS);
+        let actual = antinodes_amount(TEST_INPUT_CONTENTS);
         assert_eq!(expected, actual);
     }
 }
