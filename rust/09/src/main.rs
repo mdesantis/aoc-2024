f2c42f7633e30ca73bce120c4b0be33c2b3c1056
Maurizio De Santis <desantis.maurizio@gmail.com>
2024-12-12 19:40:31 +0100

    Add solution for day 9 part 2 (Rust)

--- rust/09/src/main.rs
+++ rust/09/src/main.rs
@@ -1,3 +1,5 @@
+#![feature(let_chains)]
+
 const INPUT_CONTENTS: &str = include_str!("../../../inputs/09/input");
 
 #[derive(Debug)]
@@ -67,9 +69,119 @@ fn filesystem_checksum_after_file_blocks_compacting(input_contents: &str) -> i64
         .sum::<i64>()
 }
 
+#[derive(Debug)]
+struct File {
+    start_index: usize,
+    size: usize,
+}
+
+fn files(blocks: &mut Vec<BlockEntry>) -> Vec<File> {
+    let mut iter = blocks
+        .iter()
+        .enumerate()
+        .filter_map(|(i, v)| match v {
+            BlockEntry::FileId(file_id) => Some((i, *file_id)),
+            _ => None,
+        })
+        .rev()
+        .peekable();
+    let mut vec = vec![];
+    let mut size = 1;
+
+    while let Some((i, file_id)) = iter.next() {
+        if let Some((_, peek_file_id)) = iter.peek() {
+            if file_id == *peek_file_id {
+                size += 1;
+            } else {
+                vec.push(File {
+                    size,
+                    start_index: i,
+                });
+                size = 1;
+            }
+        } else {
+            vec.push(File {
+                size,
+                start_index: i,
+            });
+        }
+    }
+
+    vec
+}
+
+fn free_spaces_suitable_for_compacting_start_index(
+    blocks: &mut Vec<BlockEntry>,
+    file: &File,
+) -> Option<usize> {
+    let size = file.size;
+    let mut iter = blocks.iter().enumerate();
+    let mut count = 0;
+    let mut start_index = None;
+
+    while let Some((i, v)) = iter.next() {
+        if matches!(v, BlockEntry::FreeSpace) {
+            count += 1;
+
+            if start_index.is_none() {
+                start_index = Some(i);
+            }
+
+            if size == count {
+                return start_index;
+            }
+        } else {
+            count = 0;
+            start_index = None;
+        }
+    }
+
+    None
+}
+
+fn compact_files(blocks: &mut Vec<BlockEntry>) {
+    let files = files(blocks);
+
+    for file in files {
+        let maybe_free_space_start_index =
+            free_spaces_suitable_for_compacting_start_index(blocks, &file);
+
+        if let Some(free_space_start_index) = maybe_free_space_start_index
+            && file.start_index > free_space_start_index
+        {
+            swap_file_blocks_with_free_spaces(blocks, &file, free_space_start_index);
+        }
+    }
+}
+
+fn swap_file_blocks_with_free_spaces(blocks: &mut Vec<BlockEntry>, file: &File, fsi: usize) {
+    for (file_block_index, free_space_index) in
+        (file.start_index..(file.start_index + file.size)).zip(fsi..(fsi + file.size))
+    {
+        blocks.swap(file_block_index, free_space_index);
+    }
+}
+
+fn filesystem_checksum_after_files_compacting(input_contents: &str) -> i64 {
+    let mut blocks = blocks(input_contents);
+
+    compact_files(&mut blocks);
+    blocks
+        .iter()
+        .enumerate()
+        .filter_map(|(i, v)| match v {
+            BlockEntry::FileId(id) => Some((i as i64) * id),
+            _ => None,
+        })
+        .sum::<i64>()
+}
+
 fn main() {
     let result = filesystem_checksum_after_file_blocks_compacting(INPUT_CONTENTS);
     println!("Filesystem checksum after file blocks compacting: {result}");
+
+    let result = filesystem_checksum_after_files_compacting(INPUT_CONTENTS);
+    println!("Filesystem checksum after files compacting: {result}");
 }
 
 #[cfg(test)]
@@ -79,9 +191,16 @@ mod tests {
     const TEST_INPUT_CONTENTS: &str = "2333133121414131402";
 
     #[test]
-    fn test_filesystem_checksum() {
+    fn test_filesystem_checksum_after_file_blocks_compacting() {
         let expected = 1928;
         let actual = filesystem_checksum_after_file_blocks_compacting(TEST_INPUT_CONTENTS);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_filesystem_checksum_after_files_compacting() {
+        let expected = 2858;
+        let actual = filesystem_checksum_after_files_compacting(TEST_INPUT_CONTENTS);
         assert_eq!(expected, actual);
     }
 }
